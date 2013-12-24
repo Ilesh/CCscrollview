66196b411f6efe05fd6f0f70d76f51b1221cc6f3
Omnipresent <Omnipresent@Omnipresents-Mac-mini.local>
2013-12-24 12:37:11 +0530

    Initial Commit

---
+++ myscrollview/HelloWorldLayer.h
@@ -0,0 +1,23 @@
+//
+//  HelloWorldLayer.h
+//  myscrollview
+//
+//  Created by Omnipresent on 12/24/13.
+//  Copyright Omnipresent 2013. All rights reserved.
+//
+
+
+#import <GameKit/GameKit.h>
+
+// When you import this file, you import all the cocos2d classes
+#import "cocos2d.h"
+
+// HelloWorldLayer
+@interface HelloWorldLayer : CCLayer <GKAchievementViewControllerDelegate, GKLeaderboardViewControllerDelegate>
+{
+}
+
+// returns a CCScene that contains the HelloWorldLayer as the only child
++(CCScene *) scene;
+
+@end
---
+++ myscrollview/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  myscrollview
+//
+//  Created by Omnipresent on 12/24/13.
+//  Copyright Omnipresent 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
